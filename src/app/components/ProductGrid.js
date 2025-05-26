6c818e2ab4e755db42236177502191a371466975
FatimaM05 <fatimamasoood1@gmail.com>
2025-05-26 21:24:07 +0500

    Made Product cards and edited the functionality in category page

---
+++ src/app/components/ProductGrid.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import UniversalCard from '/src/app/components/UniversalCard'
+
+function ProductGrid({products}) {
+    return (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 ml-5">
+            {products.map((p) => (
+                // Using UniversalCard component to display each product
+                //key is used to uniquely identify each element in the list
+                <UniversalCard key={p.id} product={p} />
+            ))}
+        </div>
+    )
+}
+
+export default ProductGrid
--- src/app/components/Search.js
+++ src/app/components/Search.js
@@ -1,18 +1,30 @@
 "use client";
 import React from 'react';
+import Image from 'next/image';
 
 function Search({ placeholder = "What are you looking for?", onChange }) {
-  return (
+return (
     <div className="bg-[#EDE6F6] flex items-center ml-3 
       mt-6 w-full max-w-[1240px] h-[40px] lg:h-[50px] rounded-[10px]">
-      <input
-        type="text"
-        placeholder={placeholder}
-        onChange={onChange}
-        className="bg-white w-[300px] lg:w-[400px] h-[35px] lg:h-[50px] 
-        rounded-[10px] ml-6 border-2 border-black pl-10 text-black
-        focus:outline-none focus:ring-2 focus:ring-[#8139ed]"
-      />
+      <div className='relative'>
+        <input
+          type="text"
+          placeholder={placeholder}
+          onChange={onChange}
+          className="bg-white w-[230px] lg:w-[260px] h-[35px] lg:h-[50px] 
+          rounded-[14px] ml-6 border-[1.5px] border-black pl-6 text-black
+          focus:outline-none focus:ring-2 focus:ring-[#8139ed]"
+        />
+        <div className='absolute right-2 top-1/2 transform -translate-y-1/2'>
+          <Image
+          src="/images/search-icon.png"
+          alt="Search Icon"
+          width={80}
+          height={80}
+          className='rounded-full w-6 h-6 lg:w-8 lg:h-8 cursor-pointer'
+        />
+        </div>
+      </div>
     </div>
   );
 }
